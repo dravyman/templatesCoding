2edf49efc860da1044d6fdfb07de8f86f803fe4c
dravyman <drav10@rambler.ru>
2018-12-13 14:02:50 +0300

    Zoo have own writer. So everybody can write in it
    +  Test constructor and close method;

--- src/main/java/templates/observer/Zoo.java
+++ src/main/java/templates/observer/Zoo.java
@@ -2,14 +2,23 @@ package templates.observer;
 
 import templates.strategy.Animal;
 
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Zoo {
   private List<Animal> allAnimals;
+  private Writer zooWriter;
 
-  public Zoo() {
+  public Zoo() throws IOException {
+    this(new StringWriter());
+  }
+  public Zoo(Writer writer) throws IOException {
     allAnimals = new ArrayList<>();
+    zooWriter = writer;
+    writer.append("Открытие зоопарка").append('\n');
   }
 
   public Zoo addNewAnimal(Animal newAnilmal) {
@@ -19,5 +28,14 @@ public class Zoo {
 
   public int getSize() {
     return allAnimals.size();
+  }
+
+  public Writer getWritet() {
+    return zooWriter;
+  }
+
+  public void close() throws IOException {
+    zooWriter.close();
+    zooWriter = null;
   }
 }
--- src/test/java/templates/observer/ZooTest.java
+++ src/test/java/templates/observer/ZooTest.java
@@ -1,17 +1,20 @@
 package templates.observer;
 
 import org.junit.Assert;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import templates.TestWithOwnWriter;
 import templates.strategy.Tiger;
 
-class ZooTest {
+import java.io.IOException;
+
+class ZooTest extends TestWithOwnWriter {
 
   private static Zoo myZoo;
 
-  @BeforeAll
-  static void setUp() {
-    myZoo = new Zoo();
+  @BeforeEach
+  void setUp() throws IOException {
+    myZoo = new Zoo(OUT);
   }
 
   @Test
@@ -19,5 +22,24 @@ class ZooTest {
     Assert.assertEquals(0, myZoo.getSize());
     Assert.assertEquals(1, myZoo.addNewAnimal(new Tiger()).getSize());
     Assert.assertEquals(2, myZoo.addNewAnimal(new Tiger()).getSize());
+  }
+
+  @Test
+  void testZoo() throws IOException {
+    Zoo tempZoo = new Zoo();
+    Assert.assertNotNull(tempZoo.getWritet());
+    Assert.assertEquals("Открытие зоопарка\n", tempZoo.getWritet().toString());
+    tempZoo.close();
+    Assert.assertNull(tempZoo.getWritet());
+  }
+  @Test
+  void testZooWriter() {
+    Assert.assertNotNull(myZoo.getWritet());
+    Assert.assertEquals("Открытие зоопарка\n", myZoo.getWritet().toString());
+  }
+
+  @Test
+  void testGreeting() {
+    myZoo.addNewAnimal(new Tiger());
   }
 }
